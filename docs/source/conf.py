0098f618014e5a6ffd50b1376da9d98684cb7fdd
Roberto Vera Alvarez <r78v10a07@gmail.com>
2022-05-25 10:38:38 -0400

    Updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Roberto Vera Alvarez'
 author = 'Roberto Vera Alvarez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
